7a74dbf16c64e47599273a577ba71ca4eeee47b6
Shubham Sharma <shubham_93@live.in>
2020-06-17 15:29:25 +0530

    Scrumpoker first checkin

---
+++ ScrumPoker/ScrumPoker/Services/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumPoker.Services
+{
+    public class DataService : IDataService
+    {
+
+        public void AddUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSession(Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GivePoint(string user, int points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStory(string story)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScrumPoker/ScrumPoker/Services/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumPoker.Services
+{
+    public interface IDataService
+    {
+        public void CreateSession(Guid sessionId);
+        public void AddUser(string userName);
+        public void RemoveUser(string userName);
+        public void UpdateStory(string story);
+        public void GivePoint(string user, int points);
+    }
+}
